6d3f914a4bde657d42dbd544b54adb841fb787b4
joedietrich-dev <joe@joedietrich.net>
2021-11-18 22:09:22 -0500

    commit tree e2f039b1b4efaa0fbf17e949ddc3b421d9ded960

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_113221) do
+ActiveRecord::Schema.define(version: 2021_11_19_024056) do
 
   create_table "games", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2021_07_19_113221) do
     t.integer "game_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "reviews", "users"
 end
